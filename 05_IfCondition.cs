004d3465281ce34570f7614f0af7d75212f8dcb2
brahmanand-ui <brahmanand.work@gmail.com>
2026-09-07 09:47:49 +0530

    Add  05_IfCondition.cs

---
+++ 05_IfCondition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class _05_if_statement
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("How Old You Are: ");
+        int age = int.Parse(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are eligible to vote.");
+        }
+        else
+        {
+            Console.WriteLine("You are not eligible to vote.");
+        }
+    }
+}
